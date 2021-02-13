4448a7d0e5cf0b3aafe7d080e0291018fdf73758
Семён Задорожный <simon.z@yandex.ru>
2021-02-13 03:48:22 +0300

    added list todo

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/toDoItem.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  final List<ToDoItem> toDoList = [
+    ToDoItem(name: 'Купить кофе', accept: true, startDateTime: DateTime.now()),
+    ToDoItem(name: 'Купить ёршик', accept: false, startDateTime: DateTime.now()),
+    ToDoItem(name: 'Почистить зубы', accept: true, startDateTime: DateTime.now()),
+    ToDoItem(name: 'Сварить борщ', accept: true, startDateTime: DateTime.now()),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Demo',
+      title: 'ToDo List',
       theme: ThemeData(
         primarySwatch: Colors.blue
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Шапка'),
+          title: Text('ToDo'),
           centerTitle: true,
+          backgroundColor: Colors.black45,
         ),
-        body: Center(
-          child: Text('Привет Мир!', style: TextStyle(
-            fontSize: 32,
-            color: Colors.grey[600]
-          ),),
+        body: ListView.builder(
+          padding: EdgeInsets.all(20),
+          itemCount: toDoList.length,
+          itemBuilder: (_, index) => Card(
+            child: Text(
+              '${toDoList[index].name} __ ${toDoList[index].accept}',
+              style: TextStyle(fontSize: 24),
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {print('click btn');},
+          child: Icon(Icons.adjust_outlined),
+          backgroundColor: Colors.black45,
         ),
       ),
     );
---
+++ lib/toDoItem.dart
@@ -0,0 +1,7 @@
+class ToDoItem {
+  String name;
+  bool accept;
+  DateTime startDateTime;
+
+  ToDoItem({this.name, this.accept, this.startDateTime});
+}
